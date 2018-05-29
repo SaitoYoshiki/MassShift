7e6034d82c315a4cf1086cf17c3a640b80179188
Omega2948 <yes.kestrel@gmail.com>
2018-05-29 11:47:20 +0900

    マウスカーソルをタイトルシーンに入るたびに復活するように変更

---
+++ MassShift/Assets/MouseCursorVisible.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursorVisible : MonoBehaviour {
+	void Start () {
+        Cursor.visible = true;
+	}
+}
