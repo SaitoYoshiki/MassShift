24eff65583a4c052ba2e8d2890bbeed5a2e02b9b
SaitoYoshiki <saitosaito888888@gmail.com>
2018-06-01 18:15:40 +0900

    動く床の重さが0と2のとき、ステージブロックとの間に段差ができて、プレイヤーが引っかかってしまっていたのを修正

--- MassShift/Assets/Scripts/Saito/MoveFloor.cs
+++ MassShift/Assets/Scripts/Saito/MoveFloor.cs
@@ -195,11 +195,11 @@ public class MoveFloor : MonoBehaviour {
 	Vector3 GetTargetLocalPosition() {
 		switch (GetWeight()) {
 			case WeightManager.Weight.flying:
-				return Vector3.up * (mUpHeight - 0.02f);
+				return Vector3.up * (mUpHeight - 0.0025f);
 			case WeightManager.Weight.light:
 				return Vector3.zero;
 			case WeightManager.Weight.heavy:
-				return Vector3.down * (mDownHeight - 0.02f);
+				return Vector3.down * (mDownHeight - 0.0025f);
 		}
 		Debug.LogError("ErrorWeight", this);
 		return Vector3.zero;
