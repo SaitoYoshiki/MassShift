f478290e2cdd75891ec8ff945403d548bfb1e64e
SaitoYoshiki <saitosaito888888@gmail.com>
2018-06-01 10:35:37 +0900

    デバッグオブジェクトがすべてのシーンで作成されるように変更した

---
+++ MassShift/Assets/Scripts/Saito/DebugInstanciate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugInstanciate {
+
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+	static void Init() {
+		Instantiate();
+		DisableLog();
+	}
+
+	static void Instantiate() {
+		var g = Resources.Load("DebugInstatiate") as GameObject;
+		var gi = GameObject.Instantiate(g);
+		GameObject.DontDestroyOnLoad(gi);
+	}
+
+	static void DisableLog() {
+		Debug.logger.logEnabled = false;
+	}
+}
