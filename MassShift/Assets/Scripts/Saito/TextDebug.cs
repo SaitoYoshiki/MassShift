2d1604b582be236cc37e8ca863db8e5955d8ed3e
SaitoYoshiki <saitosaito888888@gmail.com>
2018-06-08 17:30:51 +0900

    exeでデバッグしやすいようにした

---
+++ MassShift/Assets/Scripts/Saito/TextDebug.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextDebug : MonoBehaviour {
+
+	public void SetText(string aString) {
+		GetComponent<UnityEngine.UI.Text>().text = aString;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
