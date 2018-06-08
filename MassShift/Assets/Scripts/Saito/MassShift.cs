243ff5be6ec2c2cf9d0dbfc7ca4ca30b5fc477a4
SaitoYoshiki <saitosaito888888@gmail.com>
2018-06-08 21:42:50 +0900

    プレイヤーのハイライトが表示されていなかったのを修正

--- MassShift/Assets/Scripts/Saito/MassShift.cs
+++ MassShift/Assets/Scripts/Saito/MassShift.cs
@@ -1092,7 +1092,7 @@ public class MassShift : MonoBehaviour
 
 		Transform lFrame = aModel.transform.Find("Model/Hilight");
 		if (lFrame == null) {
-			lFrame = aModel.transform.Find("Offset/RotOffset/Rotation/Model/Hilight");   //プレイヤー用
+			lFrame = aModel.transform.Find("Offset/RotOffset/Rotation/ModelOffset/Model/Hilight");   //プレイヤー用
 			if (lFrame == null) return;
 		}
 
