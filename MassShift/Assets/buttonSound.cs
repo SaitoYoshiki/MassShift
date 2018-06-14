df673f1015f47d2112f63fba5f4f90d80e66b93c
Omega2948 <yes.kestrel@gmail.com>
2018-06-14 17:08:16 +0900

    UIのボタンを押した時にSEが鳴るよう変更

---
+++ MassShift/Assets/buttonSound.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonSound : MonoBehaviour {
+    [SerializeField]
+    GameObject buttonPushSound;
+
+    public void OnPush() {
+        SoundManager.SPlay(buttonPushSound);
+    }
+}
