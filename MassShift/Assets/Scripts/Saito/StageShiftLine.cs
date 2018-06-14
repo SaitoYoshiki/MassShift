99f1eb3fe8d515d41941f7ec81fc4ca43a0a7665
SaitoYoshiki <saitosaito888888@gmail.com>
2018-06-14 11:26:37 +0900

    重さを移すとどうなるかの表示を実装

---
+++ MassShift/Assets/Scripts/Saito/StageShiftLine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageShiftLine : MonoBehaviour {
+
+	[SerializeField]
+	GameObject mLinePrefab;
+
+	GameObject mSourceLine;
+	GameObject mDestLine;
+
+
+	// Use this for initialization
+	void Start() {
+		mSourceLine = Instantiate(mLinePrefab, transform);
+		mDestLine = Instantiate(mLinePrefab, transform);
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	public StageShiftLineInstance GetSource() {
+		return mSourceLine.GetComponent<StageShiftLineInstance>();
+	}
+	public StageShiftLineInstance GetDest() {
+		return mDestLine.GetComponent<StageShiftLineInstance>();
+	}
+}
---
+++ MassShift/Assets/Scripts/Saito/StageShiftLineInstance.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageShiftLineInstance : MonoBehaviour {
+
+	[SerializeField]
+	GameObject mLinePrefab;
+	
+	GameObject mLine;
+
+	[SerializeField]
+	GameObject mFail;
+
+	// Use this for initialization
+	void Awake() {
+		mLine = Instantiate(mLinePrefab, transform);
+		NotShow();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		mLine.GetComponent<MassShiftLine>().UpdatePosition();
+	}
+
+	public void ShowDown() {
+		mFail.SetActive(false);
+		mLine.SetActive(true);
+	}
+	public void ShowUp() {
+		mFail.SetActive(false);
+		mLine.SetActive(true);
+	}
+	public void ShowFail() {
+		mLine.SetActive(true);
+		mFail.SetActive(true);
+	}
+
+	public void NotShow() {
+		mLine.SetActive(false);
+		mFail.SetActive(false);
+	}
+
+
+	public void SetPosition(Vector3 aWorldPosition) {
+
+		Vector3 lStart = aWorldPosition;
+		Vector3 lEnd = lStart + Vector3.up * 1.5f;
+
+		mLine.GetComponent<MassShiftLine>().SetLinePosition(lStart, lEnd);
+
+		mFail.transform.position = lStart + Vector3.up * 1.0f;
+	}
+}
