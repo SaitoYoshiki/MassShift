ee886dcfaf483e7018142aedb2de24fa68477cef
K <ki3ttm@gmail.com>
2018-06-18 03:35:02 +0900

    水面付近で水上のオブジェクトに着地しないように修正

--- MassShift/Assets/Scripts/Tatsumi/MoveManager.cs
+++ MassShift/Assets/Scripts/Tatsumi/MoveManager.cs
@@ -573,7 +573,13 @@ public class MoveManager : MonoBehaviour {
 								(hitWaterStt && (moveVec.y < 0.0f) && hitWaterStt.IsWaterSurface && moveWeightMng.WeightLv == WeightManager.Weight.flying) ||   // 水上のオブジェクトへの水中からの着地
 								(hitLand.IsWaterFloatLanding)) {
 								if (land.GetIsLanding(Vector3.up * moveVec.y)) {
-									land.IsLanding = true;
+									// 水中や水面では水上のオブジェクトに着地しない
+									if (!(moveWaterStt && hitWaterStt && (moveWaterStt.IsInWater || moveWaterStt) && !(hitWaterStt.IsInWater || hitWaterStt.IsWaterSurface))) {
+										land.IsLanding = true;
+									}
+									else {
+										moveMng.StopMoveVirtical(MoveType.prevMove);
+									}
 								}
 								land.IsExtrusionLanding = land.GetIsLanding(Vector3.up * -moveVec.y);
 							}
