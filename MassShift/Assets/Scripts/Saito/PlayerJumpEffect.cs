ba037666ea17369e2b2a8e496e01842810a04e13
SaitoYoshiki <saitosaito888888@gmail.com>
2018-06-23 16:17:06 +0900

    ジャンプの瞬間にエフェクトを出せるようにした

---
+++ MassShift/Assets/Scripts/Saito/PlayerJumpEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpEffect : MonoBehaviour {
+
+	//このゲームオブジェクトに追従する
+	[HideInInspector]
+	public GameObject mFollowTransform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector3 lPosition = mFollowTransform.transform.position;
+		lPosition.z = 0.0f;
+		transform.position = lPosition;
+		transform.rotation = mFollowTransform.transform.rotation;
+	}
+}
