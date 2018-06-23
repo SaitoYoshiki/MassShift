3590723eb8056573588e81613d0aad21eab92085
Omega2948 <yes.kestrel@gmail.com>
2018-06-23 16:32:38 +0900

    左上のポーズUIにカーソルが乗ってるかどうかで色替えするよう変更

---
+++ MassShift/Assets/Scripts/Tamura/DisablePauseUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisablePauseUI : MonoBehaviour {
+    Result  rs;
+    Pause   ps;
+
+    [SerializeField]
+    GameObject PauseUI;
+
+	// Use this for initialization
+	void Start () {
+        rs = GetComponent<Result>();
+        ps = GetComponent<Pause>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rs.canGoal || ps.pauseFlg) {
+            PauseUI.SetActive(false);
+        }
+        else {
+            if (!PauseUI.activeSelf) {
+                PauseUI.SetActive(true);
+            }
+        }
+	}
+}
