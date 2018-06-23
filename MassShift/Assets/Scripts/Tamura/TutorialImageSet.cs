0e80f1bee305714a10e49a644e98bff95c099afc
Omega2948 <yes.kestrel@gmail.com>
2018-06-23 12:04:26 +0900

    新しいチュートリアルステージと、動画の差し替え

--- MassShift/Assets/Scripts/Tamura/TutorialImageSet.cs
+++ MassShift/Assets/Scripts/Tamura/TutorialImageSet.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// button.csからisPlayerPushでプレイヤーがボタンを押しているかどうか判定する
+
 public class TutorialImageSet : MonoBehaviour {
     public UnityEngine.UI.Image tutorialLight;
-    public UnityEngine.UI.Image tutorialVideo;
+    
+    // 最初に再生する動画
+    public UnityEngine.UI.Image tutorialVideo1;
+    UnityEngine.Video.VideoPlayer vp1;
+
+    // ボタンが押された時に再生する動画
+    public UnityEngine.UI.Image tutorialVideo2;
+    UnityEngine.Video.VideoPlayer vp2;
+
     public BoxCollider boxCol;
+
+    [SerializeField]
+    Button button;
 
     // 電源ON/OFFのアニメーション時間
     [SerializeField]
@@ -16,6 +29,13 @@ public class TutorialImageSet : MonoBehaviour {
     bool monitorAnimFlg = false;
     public bool isMonitorON = false;
 
+    void Start() {
+        vp1 = tutorialVideo1.GetComponent<UnityEngine.Video.VideoPlayer>();
+        if (tutorialVideo2 != null) {
+            vp2 = tutorialVideo2.GetComponent<UnityEngine.Video.VideoPlayer>();
+        }
+    }
+
     public void StartAnimation() {
         monitorAnimFlg = true;
         animStartTime = Time.fixedUnscaledTime;
@@ -25,17 +45,38 @@ public class TutorialImageSet : MonoBehaviour {
         float nowAnimTime = Time.fixedUnscaledTime - animStartTime;
         float animPer = Mathf.Clamp((nowAnimTime / animTime), 0.0f, 1.0f);
 
+        UnityEngine.UI.Image video;
+        UnityEngine.Video.VideoPlayer vp;
+
+        if (button != null && button.IsButtonOn && !button.IsPlayerPush && tutorialVideo2 != null) {
+            vp1.time = 0.0f;
+            vp1.Pause();
+
+            video = tutorialVideo2;
+            vp = vp2;
+        }
+        else {
+            if (tutorialVideo2 != null) {
+                vp2.time = 0.0f;
+                vp2.Pause();
+            }
+
+            video = tutorialVideo1;
+            vp = vp1;
+        }
+
         if (isMonitorON) {
             // 横広がり
-            if (tutorialVideo.transform.localScale.x < 1.0f) {
-                tutorialVideo.transform.localScale = Vector3.Lerp(new Vector3(0.0f, 0.0f, 1.0f), new Vector3(1.0f, 0.1f, 1.0f), animPer);
+            if (video.transform.localScale.x < 1.0f) {
+                video.transform.localScale = Vector3.Lerp(new Vector3(0.0f, 0.0f, 1.0f), new Vector3(1.0f, 0.1f, 1.0f), animPer);
                 if (animPer >= 1.0f) {
                     animStartTime = Time.fixedUnscaledTime;
+                    vp.Play();
                 }
             }
             // 縦広がり
             else {
-                tutorialVideo.transform.localScale = Vector3.Lerp(new Vector3(1.0f, 0.1f, 1.0f), Vector3.one, animPer);
+                video.transform.localScale = Vector3.Lerp(new Vector3(1.0f, 0.1f, 1.0f), Vector3.one, animPer);
                 if (animPer >= 1.0f) {
                     monitorAnimFlg = false;
                 }
@@ -43,17 +84,19 @@ public class TutorialImageSet : MonoBehaviour {
         }
         else {
             // 縦縮み
-            if (tutorialVideo.transform.localScale.y > 0.1f) {
-                tutorialVideo.transform.localScale = Vector3.Lerp(Vector3.one, new Vector3(1.0f, 0.1f, 1.0f), animPer);
+            if (video.transform.localScale.y > 0.1f) {
+                video.transform.localScale = Vector3.Lerp(Vector3.one, new Vector3(1.0f, 0.1f, 1.0f), animPer);
                 if (animPer >= 1.0f) {
                     animStartTime = Time.fixedUnscaledTime;
                 }
             }
             // 横縮み
             else {
-                tutorialVideo.transform.localScale = Vector3.Lerp(new Vector3(1.0f, 0.1f, 1.0f), new Vector3(0.0f, 0.0f, 1.0f), animPer);
+                video.transform.localScale = Vector3.Lerp(new Vector3(1.0f, 0.1f, 1.0f), new Vector3(0.0f, 0.0f, 1.0f), animPer);
                 if (animPer >= 1.0f) {
                     monitorAnimFlg = false;
+                    vp.time = 0.0f;
+                    vp.Pause();
                 }
             }
         }
