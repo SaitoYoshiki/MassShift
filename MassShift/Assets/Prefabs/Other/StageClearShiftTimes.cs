30587ed7814593e8bf62ad910200c53acd6dd2a8
SaitoYoshiki <saitosaito888888@gmail.com>
2018-06-26 09:15:23 +0900

    クリアするときの手数を取得できるようにした

---
+++ MassShift/Assets/Prefabs/Other/StageClearShiftTimes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scripts/StageClearShiftTimes")]
+public class StageClearShiftTimes : ScriptableObject {
+	
+	[System.Serializable]
+	class StageData {
+		[SerializeField]
+		int mGrade1 = -1;
+		[SerializeField]
+		int mGrade2 = -1;
+
+		public int Grade1 {
+			get {
+				return mGrade1;
+			}
+		}
+		public int Grade2 {
+			get {
+				return mGrade2;
+			}
+		}
+	}
+
+	[SerializeField]
+	List<StageData> mStages;
+}
